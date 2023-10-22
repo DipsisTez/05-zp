0c3c008b7c2614bc2bc1ce18cdd17af8637d6297
Kiyokan <53574596+DipsisTez@users.noreply.github.com>
2023-10-23 01:21:51 +0700

    Update _t2.py

--- _t2.py
+++ _t2.py
@@ -1,8 +1,6 @@
-def f(s):
-    return sum(int(i) for i in s)
-
 for n in range(1, 100):
     s = bin(n)[2:]  # перевод в двоичную систему
+    f = lambda x: sum(int(i) for i in s)
     s += str(f(s) % 2)
     s += str(f(s) % 2)
     r = int(s, 2)  # перевод в десятичную систему
